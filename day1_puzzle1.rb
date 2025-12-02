bddc801894e9ca0844243d06989b485988f69d40
isaacs3001 <49897129+isaacs3001@users.noreply.github.com>
2025-12-02 22:19:01 +0530

    Update day1_puzzle1.rb - puzzle_2 solution

--- day1_puzzle1.rb
+++ day1_puzzle1.rb
@@ -1,4 +1,3 @@
-
 rotations = File.readlines("day_1.txt", chomp: true)
 current_point = 50
 count = 0 
@@ -24,5 +23,30 @@ for rotation in rotations
   end
 end
 
+
+p count
+
+rotations = File.readlines("day_1.txt", chomp: true)
+current_point = 50
+count = 0 
+
+for rotation in rotations
+  operation = rotation[0]
+  shift = rotation[1..].to_i
+
+  if operation == "L"
+    shift.times do
+      current_point -= 1
+      current_point = 99 if current_point < 0
+      count += 1 if current_point == 0
+    end
+  else
+    shift.times do
+      current_point += 1
+      current_point = 0 if current_point > 99
+      count += 1 if current_point == 0
+    end
+  end
+end
 
 p count
